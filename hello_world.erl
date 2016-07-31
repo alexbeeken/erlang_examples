f9a776d78b01befc6b609d7da32b81d3eb6775a0
Alex Beeken <abeeken@focusvision.com>
2016-07-30 23:13:13 -0700

    add more method examples

--- hello_world.erl
+++ hello_world.erl
@@ -1,5 +1,26 @@
--module('elixir').
+-module('hello_world').
 -compile(export_all).
 
 hello(N) ->
   io:format("~s~n", ["give me a dollar, " ++ N ++ "."]).
+
+double(N) ->
+  N * 2.
+
+atom(1, one) ->
+  io:format("~s~n", ["atom one"]);
+
+atom(1, two) ->
+  io:format("~s~n", ["atom two"]).
+
+convert({centimeter, X}) ->
+  {inch, X / 2.54};
+
+convert({inch, Y}) -> 
+  {centimeter, Y * 2.54}.
+
+list_length([]) ->
+  0;
+
+list_length([First | Rest]) ->
+  1 + list_length(Rest).
